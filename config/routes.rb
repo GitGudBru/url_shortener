63efa28a41f0665ac46b2903b41ff93c49c8cef4
zaheen <zaheen@zaheens-MacBook-Pro.local>
2017-04-26 11:18:22 +0800

    Able to delete Url and also redirect short url to specified Url

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 	root 'urls#index'
 	resources :urls       # DO NOT FORGET TO ADD S at RESOURCESSSS
+	get '/urls/:id/short' => 'urls#short'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
