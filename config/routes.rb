985622ad4cbf1ba5ddd01dbb1a1cff1b4dc9406a
zaheen <zaheen@zaheens-MacBook-Pro.local>
2017-04-26 11:15:07 +0800

    Fixed small problem = added s at end of resource in routes.rb. Working now

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
 	root 'urls#index'
-	resource :urls
+	resources :urls       # DO NOT FORGET TO ADD S at RESOURCESSSS
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
